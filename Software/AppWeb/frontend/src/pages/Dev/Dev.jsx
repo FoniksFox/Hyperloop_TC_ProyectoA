007548d87727d2d4218b21f4d4143d9fa4d7cf5c
Foniks <borisbeslimov@gmail.com>
2025-04-19 16:08:29 +0200

    Feat(frontend)!: Set up React Router

---
+++ Software/AppWeb/frontend/src/pages/Dev/Dev.jsx
@@ -0,0 +1,12 @@
+import './Dev.css';
+
+function Dev() {
+    return (
+        <div className="dev">
+            <h1>Welcome to the Dev Page</h1>
+            <p>This is the development page of the application.</p>
+        </div>
+    );
+}
+
+export default Dev;
---
+++ Software/AppWeb/frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of the application.</p>
+        </div>
+    );
+}
+
+export default Home;
