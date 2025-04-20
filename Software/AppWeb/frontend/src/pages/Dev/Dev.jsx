0b9b6b533be15dc46702c3186e8788ce3e8df08a
Foniks <borisbeslimov@gmail.com>
2025-04-20 21:45:28 +0200

    Feat(frontend): Implement the Header into the Dev page

--- Software/AppWeb/frontend/src/pages/Dev/Dev.jsx
+++ Software/AppWeb/frontend/src/pages/Dev/Dev.jsx
@@ -1,8 +1,10 @@
 import './Dev.css';
+import Header from '../../components/Header/Header.jsx';
 
 function Dev() {
     return (
         <div className="dev">
+            <Header />
             <h1>Welcome to the Dev Page</h1>
             <p>This is the development page of the application.</p>
         </div>
