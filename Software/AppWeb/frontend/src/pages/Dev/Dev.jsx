86733d48216f6c23f177b1686ec1856c5fbe73f3
Foniks <borisbeslimov@gmail.com>
2025-04-22 21:19:21 +0200

    Feat(frontend): Add ColumnsContainer component with rezizeable columns

--- Software/AppWeb/frontend/src/pages/Dev/Dev.jsx
+++ Software/AppWeb/frontend/src/pages/Dev/Dev.jsx
@@ -1,12 +1,12 @@
 import './Dev.css';
 import Header from '../../components/Header/Header.jsx';
+import ColumnsContainer from '../../components/ColumnsContainer/ColumnsContainer.jsx';
 
 function Dev() {
     return (
         <div className="dev">
             <Header />
-            <h1>Welcome to the Dev Page</h1>
-            <p>This is the development page of the application.</p>
+            <ColumnsContainer/>
         </div>
     );
 }
