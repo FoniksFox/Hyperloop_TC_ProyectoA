3e0ec64249ef3a1ee9206d449bbfcdaab0de23c8
Foniks <borisbeslimov@gmail.com>
2025-04-24 18:08:55 +0200

    Chore(frontend)!: Setup github pages deployment

--- Software/AppWeb/frontend/src/App.jsx
+++ Software/AppWeb/frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   	return (
 		<WebSocketProvider url="ws://localhost:8765">
-			<Router>
+			<Router basename="/Hyperloop_TC_ProyectoA">
 				<Routes>
 					<Route path="/" element={<Home/>} />
 					<Route path="/dev" element={<Dev/>} />
