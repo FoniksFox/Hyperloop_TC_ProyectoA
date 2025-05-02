3c09af216ec328de0737f10cd54f8d1a2f0ce273
Boris Mladenov Beslimov <borisbeslimov@gmail.com>
2025-05-02 09:33:47 +0200

    Chore(frontend): Some CSS fixes and Sidebar component in progress

--- Software/AppWeb/frontend/src/pages/Dev/Dev.jsx
+++ Software/AppWeb/frontend/src/pages/Dev/Dev.jsx
@@ -1,12 +1,16 @@
 import './Dev.css';
 import Header from '../../components/Header/Header.jsx';
 import ColumnsContainer from '../../components/ColumnsContainer/ColumnsContainer.jsx';
+import Sidebar from '../../components/Sidebar/Sidebar.jsx';
 
 function Dev() {
     return (
         <div className="dev">
             <Header />
-            <ColumnsContainer/>
+            <div className="dev__content">
+                <Sidebar />
+                <ColumnsContainer />
+            </div>
         </div>
     );
 }
